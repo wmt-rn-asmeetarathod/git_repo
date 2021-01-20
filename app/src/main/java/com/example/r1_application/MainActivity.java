15f30fef7c6abab5e1f8df594e5e1934e598a069
wmt-rn-asmeetarathod <asmeetar@webmob.tech>
2021-01-20 10:35:51 +0530

    custom item layout Commit

--- app/src/main/java/com/example/r1_application/MainActivity.java
+++ app/src/main/java/com/example/r1_application/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.r1_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv=(RecyclerView) findViewById(R.id.rv1);
+        StaggeredGridLayoutManager sglmanager=new StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL);
+        rv.setLayoutManager(sglmanager);
     }
 }
